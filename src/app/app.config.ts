d37ba1bb63407ebd9e5135c46b454d3524fcc2f8
Marghubur <marghub12@gmail.com>
2025-01-31 21:04:18 +0530

    updated

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withHashLocation } from '@angular/router';
+import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withHashLocation())]
+  providers: [provideRouter(routes), { provide: LocationStrategy, useClass: HashLocationStrategy }]
 };
